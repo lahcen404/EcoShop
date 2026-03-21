d580c188aa7524072f5389c70ab88094caf28d5e
lahcen404 <lahcen.maskour2003@gmail.com>
2026-03-21 18:06:53 +0000

    create and fix all migrations

--- src/database/migrations/2026_03_21_172300_create_orders_table.php
+++ src/database/migrations/2026_03_21_172300_create_orders_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\OrderStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +16,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->decimal('total_price', 12, 2);
-            $table->string('status')->default('pending'); 
+            $table->string('status')->default(OrderStatus::PENDING->value);
             $table->timestamps();
         });
     }
