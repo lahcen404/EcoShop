9903fae914ea1c2a008eb15ced6c24421a66ce17
lahcen404 <lahcen.maskour2003@gmail.com>
2026-03-21 19:16:20 +0000

    create Auth Controller (Login & LogOut)

---
+++ src/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'The credentials incorrect !!!'
+            ], 401);
+        }
+
+        $token = $user->createToken('eco-shop-token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logged out successfully !!!'
+        ]);
+    }
+}
