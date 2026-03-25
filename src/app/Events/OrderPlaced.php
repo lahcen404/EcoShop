d01e2fd5400d37e6845f481b90db71af12ec3e84
lahcen404 <lahcen.maskour2003@gmail.com>
2026-03-25 12:04:52 +0100

    create the OrderPlaced Event

--- src/app/Events/OrderPlaced.php
+++ src/app/Events/OrderPlaced.php
@@ -2,6 +2,7 @@
 
 namespace App\Events;
 
+use App\Models\Order;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
@@ -14,12 +15,14 @@ class OrderPlaced
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    public $order;
+
     /**
      * Create a new event instance.
      */
-    public function __construct()
+    public function __construct(Order $order)
     {
-        //
+        $this->order = $order;
     }
 
     /**
