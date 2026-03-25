07fb1d57be05585948a84bcd7f77aec8c5a5b70e
lahcen404 <lahcen.maskour2003@gmail.com>
2026-03-25 12:07:05 +0100

    create Listener UpdateStockQte it stops the execution, and moves the task to Redis

--- src/app/Listeners/UpdateProductStock.php
+++ src/app/Listeners/UpdateProductStock.php
@@ -6,21 +6,14 @@ use App\Events\OrderPlaced;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
-class UpdateProductStock
+class UpdateProductStock implements ShouldQueue
 {
-    /**
-     * Create the event listener.
-     */
-    public function __construct()
-    {
-        //
-    }
 
-    /**
-     * Handle the event.
-     */
+
     public function handle(OrderPlaced $event): void
     {
-        //
+        foreach ($event->order->items as $item) {
+            $item->product->decrement('stock_quantity', $item->quantity);
+        }
     }
 }
